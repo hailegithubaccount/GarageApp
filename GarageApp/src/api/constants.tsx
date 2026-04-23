658d72b2137d85a5345e926aef577426aa0954d0
hailegithubaccount <michaelhaile1222@gmail.com>
2026-04-23 16:58:18 +0300

    making the api correction

--- GarageApp/src/api/constants.tsx
+++ GarageApp/src/api/constants.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // For Android emulator, use 10.0.2.2
-export const baseURL = 'http://192.168.2.165:5000/api';
+export const baseURL = 'http://localhost:5000/api';
 
 export const API = axios.create({
     baseURL: `${baseURL}/`,
